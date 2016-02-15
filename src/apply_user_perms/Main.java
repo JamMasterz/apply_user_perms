95dd7afbd3236b998adc7ba9be2eb984c6e46642
ParkerKemp <charles.kemp702@topper.wku.edu>
2016-02-15 00:38:35 -0600

    Initial commit

---
+++ src/apply_user_perms/Main.java
@@ -0,0 +1,9 @@
+package apply_user_perms;
+
+public class Main {
+	public static void main(String[] args){
+		String whitelistFilename = "whitelist.json";
+		String socketFilename = "plugins/Spinalpack/sockets/command.sock";
+		
+	}
+}
